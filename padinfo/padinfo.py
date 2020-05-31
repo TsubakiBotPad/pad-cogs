ac2b0d92015fe5ad8def56ac2e532f31d2fc3835
nachoapps <nachoappsinc@gmail.com>
2020-05-31 11:17:57 -0400

    Revert "Aobject index (#32)"
    This reverts commit 9891a345

--- padinfo/padinfo.py
+++ padinfo/padinfo.py
@@ -179,15 +179,18 @@ class PadInfo(commands.Cog):
         logger.info('Waiting until DG is ready')
         await dg_cog.wait_until_ready()
 
+        # Putting some sleeps in here to give the bot time to respond to Discord.
+        await asyncio.sleep(1)
+
         logger.info('Loading ALL index')
-        self.index_all = await dg_cog.create_index()
+        self.index_all = dg_cog.create_index()
+        await asyncio.sleep(1)
 
         logger.info('Loading NA index')
-        self.index_na = await dg_cog.create_index(lambda m: m.on_na)
+        self.index_na = dg_cog.create_index(lambda m: m.on_na)
+        await asyncio.sleep(1)
 
         logger.info('Done refreshing indexes')
-        if ctx is not None:
-            await ctx.send(inline("Done."))
 
     def get_monster_by_id(self, monster_id: int):
         dg_cog = self.bot.get_cog('Dadguide')
