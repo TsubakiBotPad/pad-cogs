e3a7a66316a481458425fb73abaa85d5ce5cdbdf
AA <chasehult@gmail.com>
2020-09-07 09:04:27 -0700

    GDPR

--- datatransfer/datatransfer.py
+++ datatransfer/datatransfer.py
@@ -24,6 +24,18 @@ class DataTransfer(commands.Cog):
         self.export = bot.get_cog("Alias").alias.command()(self.export)
         """
 
+    async def red_get_data_for_user(self, *, user_id):
+        """Get a user's personal data."""
+        data = "No data is stored for user with ID {}.\n".format(user_id)
+        return {"user_data.txt": BytesIO("data".encode())}
+
+    async def red_delete_data_for_user(self, *, requester, user_id):
+        """Delete a user's personal data.
+
+        No personal data is stored in this cog.
+        """
+        return
+
     @commands.group(name="export")
     @commands.guild_only()
     @checks.mod_or_permissions(administrator=True)
