eeb136148d07d9475e710795ce6d6b2c7f4afd0a
AA <chasehult@gmail.com>
2020-09-07 22:51:54 -0700

    Update rpadutils.py

--- rpadutils/rpadutils.py
+++ rpadutils/rpadutils.py
@@ -229,7 +229,7 @@ def default_check(payload):
     try:
         return not payload.member.bot
     except Exception as e:
-        print("DEBUG: "+e)
+        print("DEBUG:",e)
         print(payload)
         raise e
 
