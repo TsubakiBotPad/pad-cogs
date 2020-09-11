86a5a3db7efb95f9986dcc6da5e53046bc0c28ec
AA <chasehult@gmail.com>
2020-09-10 23:15:28 -0700

    remove non-padcogs

--- seniority/__init__.py
+++
@@ -1,8 +0,0 @@
-from .seniority import *
-
-__red_end_user_data_statement__ = "Activity data is stored."
-
-def setup(bot):
-    n = Seniority(bot)
-    bot.add_cog(n)
-    bot.loop.create_task(n.init())
--- translate/__init__.py
+++
@@ -1,8 +0,0 @@
-from .translate import Translate
-
-__red_end_user_data_statement__ = "No personal data is stored."
-
-def setup(bot):
-    n=Translate(bot)
-    bot.add_cog(n)
-    bot.loop.create_task(n.build_service())
