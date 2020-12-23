826fdc564b246632e6f73d3477005df03acc607b
AA <chasehult@gmail.com>
2020-12-23 01:57:11 -0800

    Remove Padvision

--- padvision/__init__.py
+++
@@ -1,7 +0,0 @@
-from .padvision import PadVision, NeuralClassifierBoardExtractor
-
-__red_end_user_data_statement__ = "No personal data is stored."
-
-
-def setup(bot):
-    bot.add_cog(PadVision(bot))
