05b6d84c0afb0dafb79111e75995b8468336ab01
TsubakiBotPad <69992611+TsubakiBotPad@users.noreply.github.com>
2021-01-07 17:35:08 +0800

    Adding buttoninfo command (#402)

--- dadguide/models/monster_stats.py
+++ dadguide/models/monster_stats.py
@@ -4,47 +4,36 @@ StatType = Literal['hp', 'atk', 'rcv']
 
 
 class MonsterStatModifierInput:
-    NUM_ATK = 0
-    NUM_HP = 0
-    NUM_RCV = 0
-    NUM_ATKpp = 0
-    NUM_HPpp = 0
-    NUM_RCVpp = 0
-    NUM_ALL_STAT = 0
-    NUM_ATK_AWAKENING = 0
-    NUM_HP_AWAKENING = 0
-    NUM_RCV_AWAKENING = 0
-
     def __init__(self, num_atk=0, num_hp=0, num_rcv=0, num_atkpp=0, num_hppp=0, num_rcvpp=0, num_all_stat=0,
                  num_atk_awakening=0, num_hp_awakening=0, num_rcv_awakening=0, num_voice_awakening=0):
-        self.NUM_ATK = num_atk
-        self.NUM_HP = num_hp
-        self.NUM_RCV = num_rcv
-        self.NUM_ATKpp = num_atkpp
-        self.NUM_HPpp = num_hppp
-        self.NUM_RCVpp = num_rcvpp
-        self.NUM_ALL_STAT = num_all_stat
-        self.NUM_ATK_AWAKENING = num_atk_awakening
-        self.NUM_HP_AWAKENING = num_hp_awakening
-        self.NUM_RCV_AWAKENING = num_rcv_awakening
-        self.NUM_VOICE_AWAKENING = num_voice_awakening
+        self.num_atk = num_atk
+        self.num_hp = num_hp
+        self.num_rcv = num_rcv
+        self.num_atkpp = num_atkpp
+        self.num_hppp = num_hppp
+        self.num_rcvpp = num_rcvpp
+        self.num_all_stat = num_all_stat
+        self.num_atk_awakening = num_atk_awakening
+        self.num_hp_awakening = num_hp_awakening
+        self.num_rcv_awakening = num_rcv_awakening
+        self.num_voice_awakening = num_voice_awakening
 
     def get_latent_multiplier(self, key: StatType):
         if key == 'hp':
-            return self.NUM_HP * 0.015 + self.NUM_HPpp * 0.045 + self.NUM_ALL_STAT * 0.03
+            return self.num_hp * 0.015 + self.num_hppp * 0.045 + self.num_all_stat * 0.03
         elif key == 'atk':
-            return self.NUM_ATK * 0.01 + self.NUM_ATKpp * 0.03 + self.NUM_ALL_STAT * 0.02
+            return self.num_atk * 0.01 + self.num_atkpp * 0.03 + self.num_all_stat * 0.02
         elif key == 'rcv':
-            return self.NUM_RCV * 0.1 + self.NUM_RCVpp * 0.3 + self.NUM_ALL_STAT * 0.2
+            return self.num_rcv * 0.1 + self.num_rcvpp * 0.3 + self.num_all_stat * 0.2
         return 0
 
     def get_awakening_addition(self, key: StatType):
         if key == 'hp':
-            return 500 * self.NUM_HP_AWAKENING
+            return 500 * self.num_hp_awakening
         elif key == 'atk':
-            return 100 * self.NUM_ATK_AWAKENING
+            return 100 * self.num_atk_awakening
         elif key == 'rcv':
-            return 200 * self.NUM_RCV_AWAKENING
+            return 200 * self.num_rcv_awakening
         return 0
 
 
@@ -58,7 +47,7 @@ class MonsterStats:
         if stat_latents:
             latents = s_val * stat_latents.get_latent_multiplier(key)
             stat_awakenings = stat_latents.get_awakening_addition(key)
-            voice = s_val * stat_latents.NUM_VOICE_AWAKENING * 0.1
+            voice = s_val * stat_latents.num_voice_awakening * 0.1
             s_val += latents + stat_awakenings + voice
 
         # include plus calculations. todo: is there a way to do this without subtraction?
