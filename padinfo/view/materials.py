adc0fa0b40fda0a005deefc7ebd4819180aba22a
AA <chasehult@gmail.com>
2021-01-23 00:02:28 -0700

    Requested Changes

--- padinfo/view/materials.py
+++ padinfo/view/materials.py
@@ -12,40 +12,28 @@ from padinfo.view.components.monster.image import MonsterImage
 if TYPE_CHECKING:
     from dadguide.models.monster_model import MonsterModel
 
-MAX_SKILLUPS_TO_SHOW = 5
-MAX_MATFOR_TO_SHOW = 5
+MAX_MONS_TO_SHOW = 5
 
-def mat_use_field(usedin, title, overflow=None):
+
+def mat_use_field(usedin, title, overflow=0):
     if not usedin:
         return EmbedField(
             title,
             Box("None"))
-    elif len(usedin) > MAX_MATFOR_TO_SHOW and overflow is None:
-        return EmbedField(
-            title,
-            Box(f"{len(usedin)} monsters"))
-    elif overflow and len(usedin) > overflow:
+    if len(usedin) > MAX_MONS_TO_SHOW:
         return EmbedField(
             title,
             Box(
                 *(MonsterHeader.short_with_emoji(em) for em in usedin[:overflow]),
-                f"({len(usedin) - overflow} more {title.lower()})"))
-    else:
-        return EmbedField(
-            title,
-            Box(*(MonsterHeader.short_with_emoji(em) for em in usedin)))
+                f"({len(usedin) - overflow} more {title.lower()})" if overflow else f"{len(usedin)} monsters"))
+    return EmbedField(
+        title,
+        Box(*(MonsterHeader.short_with_emoji(em) for em in usedin)))
 
 
 class MaterialView:
     @staticmethod
     def embed(m: "MonsterModel", mats, usedin, gemusedin, skillups, color):
-        fields = [mat_use_field(mats, "Evo materials")]
-        if usedin:
-            fields.append(mat_use_field(usedin, "Material for"))
-        if gemusedin:
-            fields.append(mat_use_field(gemusedin, "Evo gem is mat for"))
-        fields.append(mat_use_field(skillups, "Skillups", MAX_SKILLUPS_TO_SHOW))
-
         return EmbedView(
             EmbedMain(
                 color=color,
@@ -54,5 +42,10 @@ class MaterialView:
             ),
             embed_thumbnail=EmbedThumbnail(MonsterImage.icon(m)),
             embed_footer=pad_info_footer(),
-            embed_fields=fields
+            embed_fields=[f for f in [
+                mat_use_field(mats, "Evo materials"),
+                mat_use_field(usedin, "Material for") if usedin else None,
+                mat_use_field(gemusedin, "Evo gem is mat for") if gemusedin else None,
+                mat_use_field(skillups, "Skillups", MAX_MONS_TO_SHOW)
+            ] if f is not None]
         )
