2ebd349273baae84603b2b8cb63cb7002339ba35
AA <chasehult@gmail.com>
2021-01-23 06:35:19 -0700

    Completely remove Chrono Ma:Gia cog (#572)

--- chronomagia/__init__.py
+++
@@ -1,9 +0,0 @@
-from .chronomagia import ChronoMagia
-
-__red_end_user_data_statement__ = "No personal data is stored."
-
-
-def setup(bot):
-    n = ChronoMagia(bot)
-    bot.add_cog(n)
-    bot.loop.create_task(n.reload_cm_task())
