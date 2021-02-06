1280389457ed26dfca63ae94751a939d2ac1e56d
Turtle <puzzleandturtles@gmail.com>
2021-02-05 23:49:53 -0500

    Update transforminfo query

--- padinfo/core/transforminfo.py
+++ padinfo/core/transforminfo.py
@@ -4,14 +4,20 @@ from padinfo.core.id import get_monster_misc_info
 
 async def perform_transforminfo_query(dgcog, raw_query, beta_id3):
     db_context = dgcog.database
-    bm, err, debug_info = await findMonsterCustom2(dgcog, beta_id3, raw_query)
+    mgraph = dgcog.database.graph
+    m, err, debug_info = await findMonsterCustom2(dgcog, beta_id3, raw_query)
+    altversions = mgraph.process_alt_versions(m.monster_id)
 
-    if not bm:
-        return bm, err, debug_info, None, None, None, None # ????
-
-    tfm = dgcog.database.graph.get_monster(dgcog.database.graph.get_next_transform_id_by_monster(bm))
+    for mid in sorted(altversions):
+        if mgraph.monster_is_transform_base_by_id(mid):
+            tfm = mgraph.get_monster(mgraph.get_next_transform_id_by_monster_id(mid))
+            if tfm:
+                bm = mgraph.get_monster(mid)
+                break
 
     if not tfm:
-        return bm, err, debug_info, tfm, None, None, None
+        return m, err, debug_info, None, None, None, None # ????
+
+    acquire_raw, _, base_rarity, _, true_evo_type_raw = await get_monster_misc_info(db_context, tfm)
 
     return bm, err, debug_info, tfm, base_rarity, acquire_raw, true_evo_type_raw
