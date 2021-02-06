f07bb52e844c3ace10c6281c9b7ecf5be4b04e7d
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-02-06 17:53:48 -0600

    Convert Id views to using new menu code (#750)

--- padinfo/view_state/pantheon.py
+++ padinfo/view_state/pantheon.py
@@ -11,6 +11,7 @@ if TYPE_CHECKING:
 class PantheonViewState(ViewState):
     def __init__(self, original_author_id, menu_type, raw_query, query, color, monster: "MonsterModel",
                  pantheon_list: List["MonsterModel"], series_name: str,
+                 use_evo_scroll: bool = True,
                  extra_state=None):
         super().__init__(original_author_id, menu_type, raw_query, extra_state=extra_state)
         self.series_name = series_name
@@ -18,12 +19,15 @@ class PantheonViewState(ViewState):
         self.color = color
         self.monster = monster
         self.query = query
+        self.use_evo_scroll = use_evo_scroll
 
     def serialize(self):
         ret = super().serialize()
         ret.update({
+            'pane_type': 'pantheon',
             'query': self.query,
             'resolved_monster_id': self.monster.monster_id,
+            'use_evo_scroll': str(self.use_evo_scroll),
         })
         return ret
 
@@ -31,30 +35,34 @@ class PantheonViewState(ViewState):
     async def deserialize(dgcog, user_config: UserConfig, ims: dict):
         raw_query = ims['raw_query']
 
-        # This is to support the 2 vs 1 monster query difference between ^ls and ^id
-        query = ims.get('query') or raw_query
-
-        original_author_id = ims['original_author_id']
-        menu_type = ims['menu_type']
-
-        resolved_monster_id = ims.get('resolved_monster_id')
+        resolved_monster_id = int(ims.get('resolved_monster_id'))
 
         monster = await (get_monster_by_id(dgcog, resolved_monster_id)
                          if resolved_monster_id else get_monster_by_query(dgcog, raw_query, user_config.beta_id3))
 
         pantheon_list, series_name = await PantheonViewState.query(dgcog, monster)
 
+        # This is to support the 2 vs 1 monster query difference between ^ls and ^id
+        query = ims.get('query') or raw_query
+
+        original_author_id = ims['original_author_id']
+        use_evo_scroll = ims.get('use_evo_scroll') != 'False'
+        menu_type = ims['menu_type']
+
         return PantheonViewState(original_author_id, menu_type, raw_query, query, user_config.color, monster,
-                            pantheon_list, series_name,
-                            extra_state=ims)
+                                 pantheon_list, series_name,
+                                 use_evo_scroll=use_evo_scroll,
+                                 extra_state=ims)
 
     @staticmethod
     async def query(dgcog, monster):
         db_context = dgcog.database
         full_pantheon = db_context.get_monsters_by_series(monster.series_id)
+        if not full_pantheon:
+            return None, None
         pantheon_list = list(filter(lambda x: db_context.graph.monster_is_base(x), full_pantheon))
         if len(pantheon_list) == 0 or len(pantheon_list) > 20:
-            return None
+            return None, None
 
         series_name = monster.series.name_en
 
--- padinfo/view_state/pic.py
+++ padinfo/view_state/pic.py
@@ -10,17 +10,21 @@ if TYPE_CHECKING:
 
 class PicViewState(ViewState):
     def __init__(self, original_author_id, menu_type, raw_query, query, color, monster: "MonsterModel",
+                 use_evo_scroll: bool = True,
                  extra_state=None):
         super().__init__(original_author_id, menu_type, raw_query, extra_state=extra_state)
         self.color = color
         self.monster = monster
         self.query = query
+        self.use_evo_scroll = use_evo_scroll
 
     def serialize(self):
         ret = super().serialize()
         ret.update({
+            'pane_type': 'pic',
             'query': self.query,
             'resolved_monster_id': self.monster.monster_id,
+            'use_evo_scroll': str(self.use_evo_scroll),
         })
         return ret
 
@@ -28,16 +32,18 @@ class PicViewState(ViewState):
     async def deserialize(dgcog, user_config: UserConfig, ims: dict):
         raw_query = ims['raw_query']
 
-        # This is to support the 2 vs 1 monster query difference between ^ls and ^id
-        query = ims.get('query') or raw_query
-
-        original_author_id = ims['original_author_id']
-        menu_type = ims['menu_type']
-
-        resolved_monster_id = ims.get('resolved_monster_id')
+        resolved_monster_id = int(ims.get('resolved_monster_id'))
 
         monster = await (get_monster_by_id(dgcog, resolved_monster_id)
                          if resolved_monster_id else get_monster_by_query(dgcog, raw_query, user_config.beta_id3))
 
+        # This is to support the 2 vs 1 monster query difference between ^ls and ^id
+        query = ims.get('query') or raw_query
+
+        original_author_id = ims['original_author_id']
+        use_evo_scroll = ims.get('use_evo_scroll') != 'False'
+        menu_type = ims['menu_type']
+
         return PicViewState(original_author_id, menu_type, raw_query, query, user_config.color, monster,
+                            use_evo_scroll=use_evo_scroll,
                             extra_state=ims)
