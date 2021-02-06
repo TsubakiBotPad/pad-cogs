adfe0f75ecff6f6a4bdf53f0e09206fd57446b6b
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-02-05 21:07:47 -0600

    Refactor new menu view states to not re-query

--- padinfo/padinfo.py
+++ padinfo/padinfo.py
@@ -28,12 +28,12 @@ from padinfo.core.button_info import button_info
 from padinfo.core.find_monster import find_monster, findMonster1, findMonster3, \
     findMonsterCustom, calc_ratio_name, calc_ratio_modifier
 from padinfo.core.historic_lookups import historic_lookups
-from padinfo.core.id import perform_id_query
+from padinfo.core.id import get_monster_by_query, get_id_view_state_data
 from padinfo.core.leader_skills import perform_leaderskill_query
 from padinfo.core.padinfo_settings import settings
 from padinfo.emojiupdaters import IdEmojiUpdater, ScrollEmojiUpdater
-from padinfo.id_menu_old import IdMenu as IdMenuOld
 from padinfo.id_menu import IdMenu, emoji_button_names as id_menu_emoji_button_names
+from padinfo.id_menu_old import IdMenu as IdMenuOld
 from padinfo.ls_menu import LeaderSkillMenu, emoji_button_names as ls_menu_emoji_button_names
 from padinfo.view.components.monster.header import MonsterHeader
 from padinfo.view_state.id import IdViewState
@@ -190,23 +190,24 @@ class PadInfo(commands.Cog):
     @checks.bot_has_permissions(embed_links=True)
     async def _id(self, ctx, *, query: str):
         """Monster info (main tab)"""
-        if await self.config.user(ctx.author).beta_id3():
-            await self._do_id3(ctx, query)
-        else:
-            await self._do_id(ctx, query)
+        # if await self.config.user(ctx.author).beta_id3():
+        #     await self._do_id3(ctx, query)
+        # else:
+        #     await self._do_id(ctx, query)
 
-        # dgcog = await self.get_dgcog()
-        # raw_query = query
-        # color = await self.get_user_embed_color(ctx)
-        # original_author_id = ctx.message.author.id
-        # friend_cog = self.bot.get_cog("Friend")
-        # friends = (await friend_cog.get_friends(original_author_id)) if friend_cog else []
-        # monster, transform_base, true_evo_type_raw, acquire_raw, base_rarity, alt_monsters = \
-        #     await perform_id_query(dgcog, raw_query, await self.config.user(ctx.author).beta_id3())
-        # state = IdViewState(original_author_id, IdMenu.MENU_TYPE, raw_query, query, color,
-        #                     monster, transform_base, true_evo_type_raw, acquire_raw, base_rarity, alt_monsters)
-        # menu = IdMenu.menu(original_author_id, friends, self.bot.user.id)
-        # await menu.create(ctx, state)
+        dgcog = await self.get_dgcog()
+        raw_query = query
+        color = await self.get_user_embed_color(ctx)
+        original_author_id = ctx.message.author.id
+        friend_cog = self.bot.get_cog("Friend")
+        friends = (await friend_cog.get_friends(original_author_id)) if friend_cog else []
+        monster = await get_monster_by_query(dgcog, raw_query, await self.config.user(ctx.author).beta_id3())
+        transform_base, true_evo_type_raw, acquire_raw, base_rarity, alt_monsters = \
+            await get_id_view_state_data(dgcog, monster)
+        state = IdViewState(original_author_id, IdMenu.MENU_TYPE, raw_query, query, color,
+                            monster, transform_base, true_evo_type_raw, acquire_raw, base_rarity, alt_monsters)
+        menu = IdMenu.menu(original_author_id, friends, self.bot.user.id)
+        await menu.create(ctx, state)
 
     @commands.command(aliases=["idold", "oldid"])
     @checks.bot_has_permissions(embed_links=True)
--- padinfo/view_state/materials.py
+++ padinfo/view_state/materials.py
@@ -1,7 +1,8 @@
 from typing import List, TYPE_CHECKING, Optional
 
 from padinfo.common.config import UserConfig
-from padinfo.core.id import perform_mats_query
+from padinfo.common.external_links import ilmina_skill
+from padinfo.core.id import get_monster_by_id, get_monster_by_query
 from padinfo.view_state.base import ViewState
 
 if TYPE_CHECKING:
@@ -43,7 +44,43 @@ class MaterialsViewState(ViewState):
 
         query = ims.get('query') or raw_query
 
-        monster, mats, usedin, gemid, gemusedin, skillups, skillup_evo_count, link = await perform_mats_query(dgcog, query, user_config.beta_id3)
+        resolved_monster_id = ims.get('resolved_monster_id')
+
+        monster = await (get_monster_by_id(dgcog, resolved_monster_id)
+                         if resolved_monster_id else get_monster_by_query(dgcog, raw_query, user_config.beta_id3))
+
+        mats, usedin, gemid, gemusedin, skillups, skillup_evo_count, link = await MaterialsViewState.query(dgcog, monster)
 
         return MaterialsViewState(original_author_id, menu_type, raw_query, query, user_config.color, monster,
                                   mats, usedin, gemid, gemusedin, skillups, skillup_evo_count, link, extra_state=ims)
+
+    @staticmethod
+    async def query(dgcog, monster):
+        db_context = dgcog.database
+        mats = db_context.graph.evo_mats_by_monster(monster)
+        usedin = db_context.graph.material_of_monsters(monster)
+        evo_gem = db_context.graph.evo_gem_monster(monster)
+        gemid = str(evo_gem.monster_no_na) if evo_gem else None
+        gemusedin = db_context.graph.material_of_monsters(evo_gem) if evo_gem else []
+        skillups = []
+        skillup_evo_count = 0
+        link = ilmina_skill(monster)
+
+        if monster.active_skill:
+            sums = [m for m in db_context.get_monsters_by_active(monster.active_skill.active_skill_id)
+                    if db_context.graph.monster_is_farmable_evo(m)]
+            sugs = [db_context.graph.evo_gem_monster(su) for su in sums]
+            vsums = []
+            for su in sums:
+                if not any(susu in vsums for susu in db_context.graph.get_alt_monsters(su)):
+                    vsums.append(su)
+            skillups = [su for su in vsums
+                        if db_context.graph.monster_is_farmable_evo(su) and
+                        db_context.graph.get_base_id(su) != db_context.graph.get_base_id(monster) and
+                        su not in sugs] if monster.active_skill else []
+            skillup_evo_count = len(sums) - len(vsums)
+
+        if not any([mats, usedin, gemusedin, skillups and not monster.is_stackable]):
+            return None, None, None, None, None, None, None, None
+
+        return mats, usedin, gemid, gemusedin, skillups, skillup_evo_count, link
