f301a171f308872fabf1dfb6da38f4abc2c3cf6e
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-02-06 19:08:27 -0600

    Fix ls menu not working

--- padinfo/view_state/common.py
+++ padinfo/view_state/common.py
@@ -3,11 +3,11 @@ from padinfo.core.find_monster import findMonsterCustom2
 
 
 async def get_monster_from_ims(dgcog, user_config: UserConfig, ims: dict):
-    raw_query = ims['raw_query']
+    query = ims.get('query') or ims['raw_query']
 
     resolved_monster_id_str = ims.get('resolved_monster_id')
     resolved_monster_id = int(resolved_monster_id_str) if resolved_monster_id_str else None
     if resolved_monster_id:
         return dgcog.database.graph.get_monster(resolved_monster_id)
-    monster, _, _ = await findMonsterCustom2(dgcog, user_config.beta_id3, raw_query)
+    monster, _, _ = await findMonsterCustom2(dgcog, user_config.beta_id3, query)
     return monster
