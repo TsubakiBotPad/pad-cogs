0ac886bb1b9e848aa56a80144a9a3d3c8b67f3ab
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-02-26 17:26:30 -0600

    Add prev/next monster buttons to SeriesScroll (#897)
    * Fix stash pop merge conflict
    
    * Create get_current_indices method
    
    * Refactor panes so I can have multiple child responses
    
    * Refactored series scroll a bit & fixed monster list
    
    * Rewrite without up/down, lazy buttons don't work yet
    
    * Make lazy buttons work
    
    * comment print
    
    * Fix view text
    
    * Current rarity
    
    * the name is bold in the header
    
    * Refactor ui
    
    * BoldText
    
    * rename functions to prev/next monster

--- padinfo/menu/common.py
+++ padinfo/menu/common.py
@@ -13,42 +13,33 @@ class MenuPanes:
 
     @classmethod
     def emoji_names(cls):
-        return [v[0] for k, v in cls.DATA.items() if v[1] not in cls.HIDDEN_EMOJIS]
+        return [k for k, v in cls.DATA.items() if v[1] not in cls.HIDDEN_EMOJIS]
 
     @classmethod
     def transitions(cls):
-        return {v[0]: k for k, v in cls.DATA.items()}
+        return {k: v[0] for k, v in cls.DATA.items() if v[0] is not None}
 
     @classmethod
     def pane_types(cls):
-        return {v[1]: k for k, v in cls.DATA.items() if v[1] and v[1] not in cls.HIDDEN_EMOJIS}
-
-    @classmethod
-    def emoji_name_to_emoji(cls, name: str):
-        for _, data_pair in cls.DATA.items():
-            if data_pair[1] == name:
-                return data_pair[0]
-        return None
-
-    @classmethod
-    def emoji_name_to_function(cls, name: str):
-        for _, data_pair in cls.DATA.items():
-            if data_pair[1] == name:
-                return data_pair[1]
-        return None
+        return {v[1]: v[0] for k, v in cls.DATA.items() if v[1] and v[1] not in cls.HIDDEN_EMOJIS}
 
     @classmethod
     def respond_to_emoji_with_parent(cls, emoji: str):
         """Only defined for menus that support having children"""
-        for _, data_pair in cls.DATA.items():
-            if data_pair[0] == emoji:
-                return data_pair[2]
-        return None
+        if cls.DATA.get(emoji) is None:
+            return None
+        return cls.DATA[emoji][0] is not None
 
     @classmethod
     def respond_to_emoji_with_child(cls, emoji: str):
         """Only defined for menus that support having children"""
-        for _, data_pair in cls.DATA.items():
-            if data_pair[0] == emoji:
-                return data_pair[3]
-        return None
+        if cls.DATA.get(emoji) is None:
+            return None
+        return cls.DATA[emoji][2] is not None
+
+    @classmethod
+    def get_child_data_func(cls, emoji: str):
+        """Only defined for menus that support having children"""
+        if cls.DATA.get(emoji) is None:
+            return None
+        return cls.DATA[emoji][2]
