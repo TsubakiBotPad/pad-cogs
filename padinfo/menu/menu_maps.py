5cb51cead05c1d3ea3432c19b5b7cb9f6cddd156
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-02-27 14:57:30 -0600

    Add ims exceptions, refactor listener more (#903)
    * Add errors, refactor listener more
    
    * Return None instead of False on invalid emoji

--- padinfo/menu/menu_maps.py
+++ padinfo/menu/menu_maps.py
@@ -1,4 +1,5 @@
 from padinfo.menu.closable_embed import ClosableEmbedMenu, ClosableEmbedMenuPanes
+from padinfo.menu.errors import InvalidImsMenuType, MissingImsMenuType
 from padinfo.menu.id import IdMenu, IdMenuPanes
 from padinfo.menu.leader_skill import LeaderSkillMenu, LeaderSkillMenuPanes
 from padinfo.menu.leader_skill_single import LeaderSkillSingleMenu, LeaderSkillSingleMenuPanes
@@ -7,7 +8,17 @@ from padinfo.menu.series_scroll import SeriesScrollMenu, SeriesScrollMenuPanes
 from padinfo.menu.simple_text import SimpleTextMenu, SimpleTextMenuPanes
 from padinfo.menu.transforminfo import TransformInfoMenu, TransformInfoMenuPanes
 
-menu_to_panes_map = {
+type_to_menu = {
+    ClosableEmbedMenu.MENU_TYPE: ClosableEmbedMenu,
+    IdMenu.MENU_TYPE: IdMenu,
+    LeaderSkillMenu.MENU_TYPE: LeaderSkillMenu,
+    LeaderSkillSingleMenu.MENU_TYPE: LeaderSkillSingleMenu,
+    MonsterListMenu.MENU_TYPE: MonsterListMenu,
+    SeriesScrollMenu.MENU_TYPE: SeriesScrollMenu,
+    SimpleTextMenu.MENU_TYPE: SimpleTextMenu,
+    TransformInfoMenu.MENU_TYPE: TransformInfoMenu,
+}
+type_to_panes = {
     ClosableEmbedMenu.MENU_TYPE: ClosableEmbedMenuPanes,
     IdMenu.MENU_TYPE: IdMenuPanes,
     LeaderSkillMenu.MENU_TYPE: LeaderSkillMenuPanes,
@@ -18,13 +29,22 @@ menu_to_panes_map = {
     TransformInfoMenu.MENU_TYPE: TransformInfoMenuPanes,
 }
 
-menu_map = {
-    ClosableEmbedMenu.MENU_TYPE: ClosableEmbedMenu,
-    IdMenu.MENU_TYPE: IdMenu,
-    LeaderSkillMenu.MENU_TYPE: LeaderSkillMenu,
-    LeaderSkillSingleMenu.MENU_TYPE: LeaderSkillSingleMenu,
-    MonsterListMenu.MENU_TYPE: MonsterListMenu,
-    SeriesScrollMenu.MENU_TYPE: SeriesScrollMenu,
-    SimpleTextMenu.MENU_TYPE: SimpleTextMenu,
-    TransformInfoMenu.MENU_TYPE: TransformInfoMenu,
-}
+
+def get_menu(ims):
+    menu_type = ims.get('menu_type')
+    if menu_type is None:
+        raise MissingImsMenuType
+    menu = type_to_menu.get(menu_type)
+    if menu is None:
+        raise InvalidImsMenuType
+    return menu.menu()
+
+
+def get_panes_class(ims):
+    menu_type = ims.get('menu_type')
+    if menu_type is None:
+        raise MissingImsMenuType
+    panes = type_to_panes.get(menu_type)
+    if panes is None:
+        raise InvalidImsMenuType
+    return panes
