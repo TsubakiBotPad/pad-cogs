0e3220b164fbd65ff3b8fd900e0aba83d098b24e
Turtle <72906298+turtleworks@users.noreply.github.com>
2021-03-03 09:46:55 -0500

    Enable further transform emojis in transforminfo (#911)
    * Actually use the list of monster ids returned from tfinfo query
    
    * Fix acquire display bug introduced in last commit
    
    * Add respond with n method and rest of emojis to tf menu
    
    * Implement extra emojis and respond_with_n for tfinfo
    
    * Dynamically choose which transform emoji to display

--- padinfo/menu/transforminfo.py
+++ padinfo/menu/transforminfo.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from discord import Message
 from discordmenu.embed.menu import EmbedMenu, EmbedControl
-from discordmenu.emoji.emoji_cache import emoji_cache
+from tsutils import char_to_emoji
 
 from padinfo.menu.common import MenuPanes, emoji_buttons
 from padinfo.view.id import IdView, IdViewState
@@ -21,10 +21,12 @@ class TransformInfoMenu:
         dgcog = data['dgcog']
         user_config = data['user_config']
 
-        ims['query'] = str(ims['b_resolved_monster_id'])
+        # base is always first
+        ims['query'] = str(ims['resolved_monster_ids'][0])
         ims['resolved_monster_id'] = None
         id_view_state = await IdViewState.deserialize(dgcog, user_config, ims)
-        id_control = TransformInfoMenu.id_control(id_view_state)
+        reaction_list = ims['reaction_list']
+        id_control = TransformInfoMenu.id_control(id_view_state, reaction_list)
         return id_control
 
     @staticmethod
@@ -32,10 +34,28 @@ class TransformInfoMenu:
         dgcog = data['dgcog']
         user_config = data['user_config']
 
-        ims['query'] = str(ims['t_resolved_monster_id'])
+        # transform is always second
+        ims['query'] = str(ims['resolved_monster_ids'][1])
         ims['resolved_monster_id'] = None
         id_view_state = await IdViewState.deserialize(dgcog, user_config, ims)
-        id_control = TransformInfoMenu.id_control(id_view_state)
+        reaction_list = ims['reaction_list']
+        id_control = TransformInfoMenu.id_control(id_view_state, reaction_list)
+        return id_control
+
+    @staticmethod
+    async def respond_with_n(message: Optional[Message], ims, **data):
+        dgcog = data['dgcog']
+        user_config = data['user_config']
+        n = TransformInfoMenuPanes.get_n_from_reaction(data['reaction'])
+        # does this work? is this check necessary? is reaction guaranteed to resolve?
+        if n is None:
+            return None
+
+        ims['query'] = str(ims['resolved_monster_ids'][n])
+        ims['resolved_monster_id'] = None
+        id_view_state = await IdViewState.deserialize(dgcog, user_config, ims)
+        reaction_list = ims['reaction_list']
+        id_control = TransformInfoMenu.id_control(id_view_state, reaction_list)
         return id_control
 
     @staticmethod
@@ -48,16 +68,17 @@ class TransformInfoMenu:
 
     @staticmethod
     def tf_control(state: TransformInfoViewState):
+        reaction_list = state.reaction_list
         return EmbedControl(
             [TransformInfoView.embed(state)],
-            [emoji_cache.get_by_name(e) for e in TransformInfoMenuPanes.emoji_names()]
+            reaction_list
         )
 
     @staticmethod
-    def id_control(state: IdViewState):
+    def id_control(state: IdViewState, reaction_list):
         return EmbedControl(
             [IdView.embed(state)],
-            [emoji_cache.get_by_name(e) for e in TransformInfoMenuPanes.emoji_names()]
+            reaction_list
         )
 
 
@@ -65,11 +86,47 @@ class TransformInfoEmoji:
     home = emoji_buttons['home']
     down = '\N{DOWN-POINTING RED TRIANGLE}'
     up = '\N{UP-POINTING RED TRIANGLE}'
+    one = char_to_emoji('1')
+    two = char_to_emoji('2')
+    three = char_to_emoji('3')
+    four = char_to_emoji('4')
+    five = char_to_emoji('5')
+    six = char_to_emoji('6')
+    seven = char_to_emoji('7')
+    eight = char_to_emoji('8')
+    nine = char_to_emoji('9')
+    ten = char_to_emoji('10')
 
 
 class TransformInfoMenuPanes(MenuPanes):
     DATA = {
-        TransformInfoEmoji.home: (TransformInfoMenu.respond_with_overview, TransformInfoView.VIEW_TYPE),
+        TransformInfoEmoji.home: (TransformInfoMenu.respond_with_overview,
+                                  TransformInfoView.VIEW_TYPE),
         TransformInfoEmoji.down: (TransformInfoMenu.respond_with_base, IdView.VIEW_TYPE),
         TransformInfoEmoji.up: (TransformInfoMenu.respond_with_transform, IdView.VIEW_TYPE),
+        TransformInfoEmoji.one: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.two: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.three: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.four: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.five: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.six: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.seven: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.eight: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.nine: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
+        TransformInfoEmoji.ten: (TransformInfoMenu.respond_with_n, IdView.VIEW_TYPE),
     }
+
+    @classmethod
+    def get_reaction_list(cls, number_of_monsters: int):
+        if number_of_monsters > 2:
+            cls.HIDDEN_EMOJIS = TransformInfoEmoji.up
+        else:
+            cls.HIDDEN_EMOJIS = TransformInfoEmoji.one
+
+        # add 1 for the home emoji
+        return cls.emoji_names()[:number_of_monsters + 1]
+
+    @classmethod
+    def get_n_from_reaction(cls, reaction):
+        # offset by 1 because of the home emoji
+        return cls.emoji_names().index(reaction) - 1 if reaction in cls.emoji_names() else None
