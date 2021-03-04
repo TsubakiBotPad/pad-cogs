135c786b2fd9e0d762b30dfdb2a9ab3b8af9becf
AA <chasehult@gmail.com>
2021-03-04 12:49:37 -0700

    Add crud commands for awoken skills (#914)

--- crud/crud.py
+++ crud/crud.py
@@ -20,6 +20,16 @@ SERIES_KEYS = {
     "series_type": None,
 }
 
+AWOKEN_SKILL_KEYS = {
+    "awoken_skill_id": -1,
+    "name_en": 'Untranslated',
+    "name_ja": 'Untranslated',
+    "name_ko": 'Untranslated',
+    "desc_en": 'Untranslated',
+    "desc_ja": 'Untranslated',
+    "desc_ko": 'Untranslated',
+}
+
 SERIES_TYPES = [
     "regular",
     "event",
@@ -91,11 +101,55 @@ class Crud(commands.Cog):
             for page in pagify(msg):
                 await ctx.send(box(page))
 
-    @commands.group(aliases=['hera-ur'])
+    @commands.group()
     @auth_check('crud')
     @commands.check(check_crud_channel)
     async def crud(self, ctx):
         """PadGuide database CRUD."""
+
+    @crud.command()
+    async def editmonsname(self, ctx, monster_id: int, *, name):
+        """Change a monster's name_en_override"""
+        if name.lower() in ["none", "null"]:
+            name = None
+        with await self.get_cursor() as cursor:
+            cursor.execute("SELECT name_en_override FROM monsters WHERE monster_id = %s", (monster_id,))
+            old_val = cursor.fetchall()
+
+        if not old_val:
+            await ctx.send("There is no monster with id: {}".format(monster_id))
+            return
+        old_val = old_val[0]['name_en_override']
+        if not await confirm_message(ctx, ("Are you sure you want to change monster #{}'s"
+                                           " english override from `{}` to `{}`?"
+                                           "").format(monster_id, old_val, name)):
+            return
+
+        sql = ('UPDATE monsters'
+               ' SET name_en_override = %s'
+               ' WHERE monster_id = %s')
+        await self.execute_write(ctx, sql, (name, monster_id))
+
+    @crud.command()
+    async def editmonsseries(self, ctx, monster_id: int, *, series_id: int):
+        """Change a monster's series_id"""
+        with await self.get_cursor() as cursor:
+            cursor.execute("SELECT series_id FROM monsters WHERE monster_id = %s", (monster_id,))
+            old_val = cursor.fetchall()
+
+        if not old_val:
+            await ctx.send("There is no monster with id: {}".format(monster_id))
+            return
+        old_val = old_val[0]['series_id']
+        if not await confirm_message(ctx, ("Are you sure you want to change monster #{}'s"
+                                           " series id from `{}` to `{}`?"
+                                           "").format(monster_id, old_val, series_id)):
+            return
+
+        sql = ('UPDATE monsters'
+               ' SET series_id = %s'
+               ' WHERE monster_id = %s')
+        await self.execute_write(ctx, sql, (series_id, monster_id))
 
     @crud.command()
     async def searchdungeon(self, ctx, *, search_text):
@@ -171,7 +225,7 @@ class Crud(commands.Cog):
 
     @series.command(name="edit")
     async def series_edit(self, ctx, series_id: int, *elements):
-        """Edit an existing series series.
+        """Edit an existing series.
 
         Valid element keys are: `name_en`, `name_ko`, `name_ja`, `series_type`
 
@@ -225,49 +279,145 @@ class Crud(commands.Cog):
         async with aiofiles.open(fn, 'w') as f:
             await f.write(json.dumps(j, indent=2, ensure_ascii=False, sort_keys=True))
 
-    @crud.command()
-    async def editmonsname(self, ctx, monster_id: int, *, name):
-        """Change a monster's name_en_override"""
-        if name.lower() in ["none", "null"]:
-            name = None
-        with await self.get_cursor() as cursor:
-            cursor.execute("SELECT name_en_override FROM monsters WHERE monster_id = %s", (monster_id,))
-            old_val = cursor.fetchall()
+    @crud.group(aliases=["awos"])
+    async def awokenskill(self, ctx):
+        """Awoken skill related commands"""
 
-        if not old_val:
-            await ctx.send("There is no monster with id: {}".format(monster_id))
+    @awokenskill.command(name="search")
+    async def awokenskill_search(self, ctx, *, search_text):
+        """Search for a awoken skill via its jp or na name"""
+        search_text = '%{}%'.format(search_text).lower()
+        sql = ('SELECT awoken_skill_id, name_en, name_ja, name_ko, desc_en,'
+               ' desc_ja, desc_ko FROM awoken_skills'
+               ' WHERE lower(name_en) LIKE %s OR lower(name_ja) LIKE %s'
+               ' ORDER BY awoken_skill_id DESC LIMIT 20')
+        await self.execute_read(ctx, sql, [search_text, search_text])
+
+    @awokenskill.command(name="add")
+    @checks.is_owner()
+    async def awokenskill_add(self, ctx, *elements):
+        """Add a new awoken skill.
+
+        Valid element keys are: `name_en`, `name_ko`, `name_ja`, `desc_en`, `desc_ko`, `desc_ja`
+
+        Example Usage:
+        [p]crud awokenskill add key1 "Value1" key2 "Value2"
+        """
+        if len(elements) % 2 != 0:
+            await ctx.send_help()
             return
-        old_val = old_val[0]['name_en_override']
-        if not await confirm_message(ctx, ("Are you sure you want to change monster #{}'s"
-                                           " english override from `{}` to `{}`?"
-                                           "").format(monster_id, old_val, name)):
-            return
+        elements = {elements[i]: elements[i + 1] for i in range(0, len(elements), 2)}
 
-        sql = ('UPDATE monsters'
-               ' SET name_en_override = %s'
-               ' WHERE monster_id = %s')
-        await self.execute_write(ctx, sql, (name, monster_id))
-
-    @crud.command()
-    async def editmonsseries(self, ctx, monster_id: int, *, series_id: int):
-        """Change a monster's series_id"""
-        with await self.get_cursor() as cursor:
-            cursor.execute("SELECT series_id FROM monsters WHERE monster_id = %s", (monster_id,))
-            old_val = cursor.fetchall()
-
-        if not old_val:
-            await ctx.send("There is no monster with id: {}".format(monster_id))
-            return
-        old_val = old_val[0]['series_id']
-        if not await confirm_message(ctx, ("Are you sure you want to change monster #{}'s"
-                                           " series id from `{}` to `{}`?"
-                                           "").format(monster_id, old_val, series_id)):
+        if not (elements and all(x in AWOKEN_SKILL_KEYS for x in elements)):
+            await ctx.send_help()
             return
 
-        sql = ('UPDATE monsters'
-               ' SET series_id = %s'
-               ' WHERE monster_id = %s')
-        await self.execute_write(ctx, sql, (series_id, monster_id))
+        if "awoken_skill_id" not in elements or not elements["awoken_skill_id"].isdigit():
+            await ctx.send("You must supply an numeric `awoken_skill_id` when adding a new awoken skill.")
+            return
+        elements["awoken_skill_id"] = int(elements["awoken_skill_id"])
+
+        EXTRAS = {
+            'adj_hp': 0,
+            'adj_atk': 0,
+            'adj_rcv': 0,
+            'tstamp': int(datetime.now().timestamp())
+        }
+        elements = {**AWOKEN_SKILL_KEYS, **EXTRAS, **elements}
+
+        key_infix = ", ".join(elements.keys())
+        value_infix = ", ".join("%s" for v in elements.values())
+        sql = ('INSERT INTO awoken_skills ({})'
+               ' VALUES ({})').format(key_infix, value_infix)
+
+        await self.execute_write(ctx, sql, (*elements.values(),))
+
+        fn = os.path.join(await self.config.pipeline_base(), 'etl/pad/storage_processor/awoken_skill.json')
+        async with aiofiles.open(fn, 'r') as f:
+            j = json.loads(await f.read())
+        j.append({
+            "adj_atk": 0,
+            "adj_hp": 0,
+            "adj_rcv": 0,
+            "desc_ja": elements['desc_ja'],
+            "desc_ja_official": "Unknown Official Text",
+            "desc_ko": elements['desc_ko'],
+            "desc_ko_official": "Unknown Official Text",
+            "desc_en": elements['desc_en'],
+            "desc_en_official": "Unknown Official Text",
+            'name_ja': elements['name_ja'],
+            "name_ja_official": "Unknown Official Name",
+            'name_en': elements['name_en'],
+            "name_ko_official": "Unknown Official Name",
+            'name_ko': elements['name_ko'],
+            "name_en_official": "Unknown Official Name",
+            "pad_awakening_id": elements['awoken_skill_id'],
+        })
+        async with aiofiles.open(fn, 'w') as f:
+            await f.write(json.dumps(j, indent=2, ensure_ascii=False, sort_keys=True))
+
+    @awokenskill.command(name="edit")
+    async def awokenskill_edit(self, ctx, awoken_skill_id, *elements):
+        """Edit an existing awoken skill.
+
+        Valid element keys are: `name_en`, `name_ko`, `name_ja`, `desc_en`, `desc_ko`, `desc_ja`
+
+        Example Usage:
+        [p]crud awokenskill edit 100 key1 "Value1" key2 "Value2"
+        """
+        if not awoken_skill_id.isdigit():
+            await ctx.send("String awoken skill lookup is not supported yet.")
+            return
+
+        awoken_skill_id = int(awoken_skill_id)
+
+        if len(elements) % 2 != 0:
+            await ctx.send_help()
+            return
+        elements = {elements[i]: elements[i + 1] for i in range(0, len(elements), 2)}
+
+        if not (elements and all(x in AWOKEN_SKILL_KEYS for x in elements)):
+            await ctx.send_help()
+            return
+
+        if 'awoken_skill_id' in elements:
+            await ctx.send("`awoken_skill_id` is not a supported key for editing.")
+            return
+
+        replacement_infix = ", ".join(["{} = %s".format(k) for k in elements.keys()])
+        sql = ('UPDATE awoken_skills'
+               ' SET {}'
+               ' WHERE awoken_skill_id = %s').format(replacement_infix)
+
+        await self.execute_write(ctx, sql, (*elements.values(), awoken_skill_id))
+
+        fn = os.path.join(await self.config.pipeline_base(), 'etl/pad/storage_processor/awoken_skill.json')
+        async with aiofiles.open(fn, 'r') as f:
+            j = json.loads(await f.read())
+
+        for e in j:
+            if e['pad_awakening_id'] == awoken_skill_id:
+                e.update(elements)
+        async with aiofiles.open(fn, 'w') as f:
+            await f.write(json.dumps(j, indent=2, ensure_ascii=False, sort_keys=True))
+
+    @awokenskill.command(name="delete")
+    @checks.is_owner()
+    async def awokenskill_delete(self, ctx, awoken_skill_id: int):
+        """Delete an existing awoken skill"""
+        sql = ('DELETE FROM awoken_skills'
+               ' WHERE awoken_skill_id = %s')
+
+        await self.execute_write(ctx, sql, awoken_skill_id)
+
+        fn = os.path.join(await self.config.pipeline_base(), 'etl/pad/storage_processor/awoken_skill.json')
+        async with aiofiles.open(fn, 'r') as f:
+            j = json.loads(await f.read())
+        for e in j[:]:
+            if e['pad_awakening_id'] == awoken_skill_id:
+                j.remove(e)
+        async with aiofiles.open(fn, 'w') as f:
+            await f.write(json.dumps(j, indent=2, ensure_ascii=False, sort_keys=True))
 
     @crud.command()
     @checks.is_owner()
