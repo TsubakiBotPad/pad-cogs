c7ad97a1e295584254e2d9af7d8b2d1f67b8a207
Turtle <puzzleandturtles@gmail.com>
2021-03-04 12:20:06 -0500

    Add awakening description and clean up code a little

--- padinfo/view/awakening_help.py
+++ padinfo/view/awakening_help.py
@@ -21,10 +21,9 @@ class AwakeningHelpViewProps:
         self.monster = monster
 
 
-def _get_awakening_desc(awakening: "AwakeningModel"):
+def _get_awakening_desc(index: int, awakening: "AwakeningModel"):
     emoji_text = get_awakening_emoji(awakening.awoken_skill_id, awakening.name)
-    # TODO
-    desc = 'Awakening long description text here'
+    desc = awakening.awoken_skill.desc_en
     return Box(
         Text(emoji_text),
         Text(desc),
@@ -34,13 +33,15 @@ def _get_awakening_desc(awakening: "AwakeningModel"):
 
 def normal_awakenings(monster: "MonsterModel"):
     normal_awakening_count = len(monster.awakenings) - monster.superawakening_count
-    awakening_descs = [_get_awakening_desc(a) for a in monster.awakenings[:normal_awakening_count]]
+    normal_awakenings = monster.awakenings[:normal_awakening_count]
+    awakening_descs = [_get_awakening_desc(i, awake) for i, awake in enumerate(normal_awakenings)]
     return Box(*awakening_descs)
 
 
 def super_awakenings(monster: "MonsterModel"):
     normal_awakening_count = len(monster.awakenings) - monster.superawakening_count
-    awakening_descs = [_get_awakening_desc(a) for a in monster.awakenings[normal_awakening_count:]]
+    super_awakenings = monster.awakenings[normal_awakening_count:]
+    awakening_descs = [_get_awakening_desc(i, awake) for i, awake in enumerate(super_awakenings)]
     return Box(*awakening_descs) if len(awakening_descs) > 0 else Box()
 
 
@@ -67,5 +68,5 @@ class AwakeningHelpView:
                 MonsterImage.icon(monster)
             ),
             embed_footer=pad_info_footer_with_state(state),
-            embed_fields=fields if monster.awakenings else None
+            embed_fields=fields
         )
