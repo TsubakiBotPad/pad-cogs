954d572602a759ccd165caf88c26e14a70aaaf2b
Turtle <72906298+turtleworks@users.noreply.github.com>
2021-03-11 01:59:41 -0500

    Guarantee a pantheon tab by filtering if needed (#921)
    * Stop using magic number in pantheon
    
    * Add filtering logic
    
    * Make pantheon filter on base monster's details
    
    * Filter pantheon by a monster's subatt if it has no main att
    
    * Refine material type filtering
    
    * Display pantheon filtering criteria
    
    * Clean up pantheon filtering code
    
    * Add base monster to pantheon view state and use it to show current position
    
    * Add filter labels to pantheon
    
    * Update pantheon failure message in padinfo
    
    * Use list comprehensions to filter pantheon
    
    * Fix bug filtering monsters with no main attribute
    
    * Add comment describing filtering logic to pantheon
    
    * Compare against enum name instead of value
    
    * Change no attribute to nil attribute
    
    * Rework filter display
    
    * Actually use correct max number of mons instead of one fewer
    
    * Condense list length comparison
    
    * Move pantheon lines method

--- padinfo/view/pantheon.py
+++ padinfo/view/pantheon.py
@@ -5,6 +5,7 @@ from discordmenu.embed.components import EmbedMain, EmbedField, EmbedThumbnail
 from discordmenu.embed.view import EmbedView
 
 from padinfo.common.config import UserConfig
+from padinfo.common.emoji_map import get_attribute_emoji_by_monster, get_attribute_emoji_by_enum
 from padinfo.common.external_links import puzzledragonx
 from padinfo.view.common import get_monster_from_ims
 from padinfo.view.components.base import pad_info_footer_with_state
@@ -16,10 +17,14 @@ from padinfo.view.id import evos_embed_field
 if TYPE_CHECKING:
     from dadguide.models.monster_model import MonsterModel
 
+MAX_MONS_TO_SHOW = 20
+MAT_TYPES = ['Evolve', 'Awoken', 'Enhance', 'Vendor']
+NIL_ATT = 'Nil'
+
 
 class PantheonViewState(ViewStateBaseId):
     def __init__(self, original_author_id, menu_type, raw_query, query, color, monster: "MonsterModel", alt_monsters,
-                 pantheon_list: List["MonsterModel"], series_name: str,
+                 pantheon_list: List["MonsterModel"], series_name: str, base_monster,
                  use_evo_scroll: bool = True,
                  reaction_list: List[str] = None,
                  extra_state=None):
@@ -29,6 +34,7 @@ class PantheonViewState(ViewStateBaseId):
                          extra_state=extra_state)
         self.series_name = series_name
         self.pantheon_list = pantheon_list
+        self.base_monster = base_monster
 
     def serialize(self):
         ret = super().serialize()
@@ -42,7 +48,7 @@ class PantheonViewState(ViewStateBaseId):
         if ims.get('unsupported_transition'):
             return None
         monster = await get_monster_from_ims(dgcog, ims)
-        pantheon_list, series_name = await PantheonViewState.query(dgcog, monster)
+        pantheon_list, series_name, base_monster = await PantheonViewState.query(dgcog, monster)
 
         if pantheon_list is None:
             return None
@@ -56,24 +62,96 @@ class PantheonViewState(ViewStateBaseId):
         reaction_list = ims.get('reaction_list')
 
         return cls(original_author_id, menu_type, raw_query, query, user_config.color, monster, alt_monsters,
-                   pantheon_list, series_name,
+                   pantheon_list, series_name, base_monster,
                    use_evo_scroll=use_evo_scroll,
                    reaction_list=reaction_list,
                    extra_state=ims)
 
     @classmethod
+    def make_series_name(cls, series_name, filter_strings):
+        return '{} [Filters: {}]'.format(series_name, ' '.join(filter_strings))
+
+    @classmethod
     async def query(cls, dgcog, monster):
+        # filtering rules:
+        # 1. don't show monsters that only have mat types (or show only mats if monster is a mat)
+        # 2. if still too many, show only monsters with the same base rarity
+        # 3. if still too many, show only monsters with the same base main attribute (any monster
+        #        with nil main attribute is compared using its subattribute)
+        # 4. if still too many, show only monsters that match both base attributes
+        # 5. if still too many, truncate (and probably redefine pantheon)
+
         db_context = dgcog.database
+        series_name = monster.series.name_en
         full_pantheon = db_context.get_monsters_by_series(monster.series_id)
         if not full_pantheon:
-            return None, None
-        pantheon_list = list(filter(lambda x: db_context.graph.monster_is_base(x), full_pantheon))
-        if len(pantheon_list) == 0 or len(pantheon_list) > 20:
-            return None, None
+            return None, None, None
 
-        series_name = monster.series.name_en
+        base_mon = db_context.graph.get_base_monster(monster)
 
-        return pantheon_list, series_name
+        base_list = [m for m in full_pantheon if db_context.graph.monster_is_base(m)]
+        if 0 < len(base_list) <= MAX_MONS_TO_SHOW:
+            return base_list, series_name, base_mon
+
+        # if monster has only mat types, show only mats, otherwise show everything else
+        type_list = []
+        if all(t.name in MAT_TYPES for t in monster.types):
+            series_name += ' (Mat)'
+            type_list = [m for m in base_list if all(t.name in MAT_TYPES for t in m.types)]
+        else:
+            type_list = [m for m in base_list if any(t.name not in MAT_TYPES for t in m.types)]
+        if 0 < len(type_list) <= MAX_MONS_TO_SHOW:
+            return type_list, series_name, base_mon
+
+        filters = []
+
+        rarity_list = [m for m in type_list if m.rarity == base_mon.rarity]
+        filters.append('{}*'.format(base_mon.rarity))
+        if 0 < len(rarity_list) <= MAX_MONS_TO_SHOW:
+            return rarity_list, cls.make_series_name(series_name, filters), base_mon
+
+        main_att = base_mon.attr1
+        sub_att = base_mon.attr2
+
+        # if the monster has nil main attribute, do comparisons using its subattribute
+        # if any monster in the list has nil main attribute, also compare using its subattribute
+        main_att_list = []
+        att_emoji = None
+        if main_att.name == NIL_ATT:
+            att_emoji = get_attribute_emoji_by_enum(sub_att)
+            main_att_list = [m for m in rarity_list if m.attr1.name == sub_att.name
+                                                       or (m.attr1.name == NIL_ATT
+                                                           and m.attr2.name == sub_att.name)]
+        else:
+            att_emoji = get_attribute_emoji_by_enum(main_att)
+            main_att_list = [m for m in rarity_list if m.attr1.name == main_att.name
+                                                       or (m.attr1.name == NIL_ATT
+                                                           and m.attr2.name == main_att.name)]
+        if 0 < len(main_att_list) <= MAX_MONS_TO_SHOW:
+            # append after check this time, because if we go to subatt we only want one emoji
+            filters.append(att_emoji)
+            return main_att_list, cls.make_series_name(series_name, filters), base_mon
+
+        sub_att_list = [m for m in main_att_list if m.attr1.name == main_att.name
+                                                    and m.attr2.name == sub_att.name]
+        filters.append(get_attribute_emoji_by_monster(base_mon))
+        if 0 < len(sub_att_list) <= MAX_MONS_TO_SHOW:
+            return sub_att_list, cls.make_series_name(series_name, filters), base_mon
+
+        # if we've managed to get here, just cut it off
+        pantheon_list = sub_att_list[:MAX_MONS_TO_SHOW]
+        filters.append('(still too many, truncated)')
+
+        return pantheon_list, cls.make_series_name(series_name, filters), base_mon
+
+
+def _pantheon_lines(monsters, base_monster):
+    if not len(monsters):
+        return []
+    return [
+        MonsterHeader.short_with_emoji(mon, link=mon.monster_id != base_monster.monster_id)
+        for mon in sorted(monsters, key=lambda x: int(x.monster_id))
+    ]
 
 
 class PantheonView:
@@ -83,10 +161,7 @@ class PantheonView:
     def embed(state: PantheonViewState):
         fields = [EmbedField(
             'Pantheon: {}'.format(state.series_name),
-            Box(
-                *[MonsterHeader.short_with_emoji(m)
-                  for m in sorted(state.pantheon_list, key=lambda x: x.monster_no_na)]
-            )
+            Box(*_pantheon_lines(state.pantheon_list, state.base_monster))
         ),
             evos_embed_field(state)]
 
