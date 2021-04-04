91d2dd70cfb52c8af7d5a4ddc0558915a9e53a69
Ethan Phan <ethananhphan@gmail.com>
2021-04-04 17:50:52 -0400

    updated monidlistener

--- idlookup/__init__.py
+++
@@ -1,7 +0,0 @@
-from .idlookup import Idlookup
-
-__red_end_user_data_statement__ = "Channels that idlookup is enabled are stored persistantly."
-
-
-def setup(bot):
-    bot.add_cog(Idlookup(bot))
---
+++ monidlistener/__init__.py
@@ -0,0 +1,7 @@
+from .monidlistener import MonIdListener
+
+__red_end_user_data_statement__ = "No personal data is stored."
+
+
+def setup(bot):
+    bot.add_cog(MonIdListener(bot))
