2f29c616f442be182b5992a500c41a28eb170f4f
AA <chasehult@gmail.com>
2021-04-30 15:08:36 -0700

    Add number-of-awakening moddedfiers (#992)

--- dadguide/models/awakening_model.py
+++ dadguide/models/awakening_model.py
@@ -7,7 +7,7 @@ class AwakeningModel(BaseModel):
     This class represents an awakening belonging to a monster, in contrast to AwokenSkillModel, which represents an "abstract" awoken skill.
     """
 
-    def __init__(self, awoken_skill_model: AwokenSkillModel = None, **kwargs):
+    def __init__(self, awoken_skill_model: AwokenSkillModel, **kwargs):
         self.awakening_id = kwargs['awakening_id']
         self.monster_id = kwargs['monster_id']
         self.awoken_skill_id = kwargs['awoken_skill_id']
@@ -20,3 +20,11 @@ class AwakeningModel(BaseModel):
         return {
             'awakening_id': self.awakening_id,
         }
+
+    def __eq__(self, other):
+        if isinstance(other, AwakeningModel):
+            return self.awakening_id == other.awakening_id
+        elif isinstance(other, AwokenSkillModel):
+            return self.awoken_skill == other
+        else:
+            return False
