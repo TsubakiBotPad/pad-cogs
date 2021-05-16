63d8eee289fca6502beb63eb85a2127324315f9c
Turtle <puzzleandturtles@gmail.com>
2021-05-16 00:56:25 -0400

    Implement parsing of which into embed fields

--- padglobal/view/which.py
+++ padglobal/view/which.py
@@ -13,11 +13,31 @@ class WhichViewProps:
         self.success = success
 
 
+def _get_field(paragraph):
+    paragraph = paragraph.strip()
+    newline = paragraph.find('\n')
+
+    if newline >= 0:
+        data = paragraph.split('\n')
+        evo = paragraph[:newline]
+        if evo.startswith('**'):
+            information = paragraph[newline:]
+            return EmbedField(evo, Box(information))
+
+    # it didn't start with a bolded line, assume it's not an evo description
+    return EmbedField('**Additional Information**', Box(paragraph))
+
+
 def get_title(name, timestamp, success):
     if success:
         return Text('Which {} - Last Updated {}'.format(name, timestamp))
     else:
         return Text('Which {}'.format(name))
+
+
+def get_fields(definition):
+    paragraphs = definition.split('\n\n')
+    return [_get_field(paragraph) for paragraph in paragraphs]
 
 
 class WhichView:
@@ -30,14 +50,13 @@ class WhichView:
         timestamp = props.timestamp
         success = props.success
 
-        fields = [
-
-        ]
+        fields = get_fields(definition) if success else []
 
         return EmbedView(
             EmbedMain(
                 color=state.color,
-                title=get_title(name, timestamp, success)
+                title=get_title(name, timestamp, success),
+                description=definition if not success else ''
             ),
             embed_footer=embed_footer_with_state(state),
             embed_fields=fields
