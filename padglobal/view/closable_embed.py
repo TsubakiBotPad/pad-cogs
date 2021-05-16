0970e08924dda21e782b2f4503f5f2cb5079657d
Turtle <puzzleandturtles@gmail.com>
2021-05-16 15:05:43 -0400

    Use ViewState directly instead of a ViewStateBase

--- padglobal/view/closable_embed.py
+++ padglobal/view/closable_embed.py
@@ -1,9 +1,12 @@
-from padglobal.view.view_state_base import ViewStateBase
+from discordmenu.embed.view_state import ViewState
 
 
-class ClosableEmbedViewState(ViewStateBase):
+class ClosableEmbedViewState(ViewState):
     def __init__(self, original_author_id, menu_type, raw_query, color, view_type, props):
         super().__init__(original_author_id, menu_type, raw_query)
         self.color = color
         self.view_type = view_type
         self.props = props
+
+    def serialize(self):
+        return super().serialize()
