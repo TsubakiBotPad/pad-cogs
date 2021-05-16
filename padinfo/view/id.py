398b911312d1535b28e71054fee57d70e00102e4
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-05-15 22:48:37 -0500

    Update help text for decorated evos (#1055)
    This finishes PR #1054 to show base equips too

--- padinfo/view/id.py
+++ padinfo/view/id.py
@@ -128,21 +128,19 @@ def _monster_is_enhance(m: "MonsterModel"):
 
 
 def evos_embed_field(state: ViewStateBaseId):
-    monster = state.monster
-    irreversible = False
-    equip = False
-    for alt_evo in state.alt_monsters:
-        if not alt_evo.evolution:
-            continue
-        elif not alt_evo.evolution.reversible:
-            irreversible = True
-        elif alt_evo.monster.is_equip:
-            equip = True
-        else:
-            continue
+    field_text = "**Evos**"
+    help_text = ""
+    # this isn't used right now, but maybe later if discord changes the api for embed titles...?
+    help_link = "https://github.com/TsubakiBotPad/pad-cogs/wiki/Evolutions-mini-view"
+    legend_parts = []
+    if any([alt_evo.evolution and not alt_evo.evolution.reversible for alt_evo in state.alt_monsters]):
+        legend_parts.append("⌊Irreversible⌋")
+    if any([alt_evo.monster.is_equip for alt_evo in state.alt_monsters]):
+        legend_parts.append("⌈Equip⌉")
+    if legend_parts:
+        help_text = ' – Help: {}'.format(" ".join(legend_parts))
     return EmbedField(
-        "**Evos**" + (" - " if irreversible or equip else "") +
-        ("⌊Irreversible⌋" if irreversible else "") + ("⌈Equip⌉" if equip else ""),
+        field_text + help_text,
         HighlightableLinks(
             links=[LinkedText(alt_fmt(me, state), puzzledragonx(me.monster)) for me in state.alt_monsters],
             highlighted=next(i for i, me in enumerate(state.alt_monsters)
