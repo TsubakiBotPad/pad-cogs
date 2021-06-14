c0d69ff82eebc729bfa56cf3a3afa6fc12392088
AA <chasehult@gmail.com>
2021-06-14 04:49:13 -0700

    [PDE] Save event loops as variables for easy deletion (#1234)

--- padevents/__init__.py
+++ padevents/__init__.py
@@ -4,7 +4,4 @@ __red_end_user_data_statement__ = "No personal data is stored."
 
 
 def setup(bot):
-    n = PadEvents(bot)
-    bot.add_cog(n)
-    bot.loop.create_task(n.reload_padevents())
-    bot.loop.create_task(n.check_started())
+    bot.add_cog(PadEvents(bot))
