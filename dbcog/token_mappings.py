ad126b51a997dfd29cd9a8bf314b1b157e04f745
AA <chasehult@gmail.com>
2021-10-01 05:16:30 -0700

    Add `dateadded` as an alias (#1468)

--- dbcog/token_mappings.py
+++ dbcog/token_mappings.py
@@ -333,7 +333,7 @@ STRING_MONSTER_ATTRIBUTE_ALIASES = {
     (('active_skill', 'name_en'),): ('asname',),
     (('active_skill', 'desc_en'),): ('asdesc',),
     (('series', 'name_en'),): ('seriesname',),
-    (('history_us',),): ('regdate',),
+    (('history_us',),): ('regdate', 'dateadded'),
 }
 STRING_MONSTER_ATTRIBUTE_NAMES = {*sum(STRING_MONSTER_ATTRIBUTE_ALIASES.values(), ())}
 
