6bad8c06f6798a4fbfafa678471fe34eae2f8374
River <18037011+RheingoldRiver@users.noreply.github.com>
2022-01-11 02:46:23 -0600

    Change transform emoji again (#1560)

--- padinfo/view/components/base_id_main_view.py
+++ padinfo/view/components/base_id_main_view.py
@@ -45,7 +45,7 @@ class BaseIdMainView(BaseIdView, ABC):
     active_skill_type_texts = {
         0: None,
         1: f"[\N{GAME DIE} Random]",
-        2: f"[\N{RIGHTWARDS ARROW WITH HOOK}\N{VARIATION SELECTOR-16} Transforms]",
+        2: f"[\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE} Transforms]",
         3: f"[\N{BLACK UNIVERSAL RECYCLING SYMBOL}\N{VARIATION SELECTOR-16} Cycles]"
     }
 
