0d3c509df956b3607b6efc7aaab19e0132ef9937
River <18037011+RheingoldRiver@users.noreply.github.com>
2022-01-26 21:50:06 -0600

    shield_amount emoji (#1574)

--- padinfo/core/leader_skills.py
+++ padinfo/core/leader_skills.py
@@ -59,7 +59,7 @@ def _format_number(val):
 
 
 def _format_ls_text(hp, atk, rcv, resist=0, combo=0, fua=0, mfua=0, te=0):
-    resist_val = ' {}{}%'.format(get_emoji('defense'), _format_number(100 * resist)) if resist else ''
+    resist_val = ' {}{}%'.format(get_emoji('shield_amount'), _format_number(100 * resist)) if resist else ''
 
     ehp = ' [{}x eHP]'.format(_format_number(hp / (1 - resist))) if resist else ''
 
