8ca1cb0c8148ee1a52c4f68c7af04dff7435eeed
AA <chasehult@gmail.com>
2022-05-05 19:00:20 -0700

    Add dungeon aliases

--- dungeoncog/__init__.py
+++ dungeoncog/__init__.py
@@ -10,5 +10,6 @@ with open(Path(__file__).parent / "info.json") as file:
 def setup(bot):
     n = DungeonCog(bot)
     bot.add_cog(n)
+    bot.loop.create_task(n.load_aliases())
     bot.loop.create_task(n.register_menu())
     bot.loop.create_task(n.load_emojis())
