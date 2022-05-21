6a337cc2e4bdb6bff2b5907b5abc0861ea80ea6a
AA <chasehult@gmail.com>
2022-05-20 17:59:19 -0700

    Rename PadGuideDB to PipelineUI (#1686)

--- pipelineui/__init__.py
+++ pipelineui/__init__.py
@@ -1,8 +1,8 @@
-from .padguidedb import PadGuideDb
+from .pipelineui import PipelineUI
 
 __red_end_user_data_statement__ = "No personal data is stored."
 
 
 def setup(bot):
-    pdb = PadGuideDb(bot)
+    pdb = PipelineUI(bot)
     bot.add_cog(pdb)
