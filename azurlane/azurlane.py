bd7b2e8a55caccd11239f36bd7e15e96a5d97465
AA <chasehult@gmail.com>
2022-05-24 02:38:19 -0700

    Fix azurlane cog (#1695)

--- azurlane/azurlane.py
+++ azurlane/azurlane.py
@@ -1,23 +1,25 @@
-import aiohttp
 import difflib
-import discord
 import json
 import logging
-from io import BytesIO
 from collections import OrderedDict
+from io import BytesIO
+
+import aiohttp
+import discord
 from redbot.core import checks, commands
-from redbot.core.utils.chat_formatting import inline
 from tsutils.emoji import char_to_emoji
 from tsutils.old_menu import EmojiUpdater, Menu
+from tsutils.tsubaki.links import CLOUDFRONT_URL
+from tsutils.user_interaction import send_cancellation_message
 
-logger = logging.getLogger('red.padbot-cogs.azurelane')
+logger = logging.getLogger('red.padbot-cogs.azurlane')
 
-BASE_URL = 'https://storage.googleapis.com/mirubot/alimages/raw'
-DATA_URL = '{}/azure_lane.json'.format(BASE_URL)
+BASE_URL = f'{CLOUDFRONT_URL}/azurlane/{{}}'
+DATA_URL = BASE_URL.format('data.json')
 
 
-class AzureLane(commands.Cog):
-    """AzureLane."""
+class AzurLane(commands.Cog):
+    """AzurLane."""
 
     def __init__(self, bot, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -47,16 +49,10 @@ class AzureLane(commands.Cog):
             async with session.get(DATA_URL) as resp:
                 raw_resp = await resp.text()
                 self.card_data = json.loads(raw_resp)['items']
-        logger.info('done retrieving cards: {}'.format(len(self.card_data)))
+        logger.info(f'Done retrieving cards: {len(self.card_data)}')
 
         self.id_to_card = {c['id']: c for c in self.card_data}
-        name_to_card = {'{}'.format(c['name_en']).lower(): c for c in self.card_data}
-        #         collection_name_to_card = {'{} {}'.format(
-        #             i['title'], c['name_en']).lower(): c for i in c['images'] for c in self.card_data}
-        self.names_to_card = {
-            **name_to_card,
-            #             **collection_name_to_card,
-        }
+        self.names_to_card = {'{}'.format(c['name_en']).lower(): c for c in self.card_data}
 
     @commands.command()
     @checks.bot_has_permissions(embed_links=True)
@@ -77,13 +73,13 @@ class AzureLane(commands.Cog):
         if c:
             await self.do_menu(ctx, c)
         else:
-            await ctx.send(inline('no matches'))
+            await send_cancellation_message(ctx, f'No matches for query `{query}`.')
 
     async def do_menu(self, ctx, c):
         emoji_to_embed = OrderedDict()
         for idx, image_info in enumerate(c['images']):
             emoji = char_to_emoji(str(idx))
-            emoji_to_embed[emoji] = make_card_embed(c, idx)
+            emoji_to_embed[emoji] = self.make_card_embed(c, idx)
         starting_menu_emoji = list(emoji_to_embed.keys())[0]
         return await self._do_menu(ctx, starting_menu_emoji, emoji_to_embed)
 
@@ -102,23 +98,20 @@ class AzureLane(commands.Cog):
         except Exception as ex:
             logger.exception('Menu failure')
 
+    @classmethod
+    def make_card_embed(cls, c, image_idx):
+        cid = c['id']
+        name = c['name_en']
+        info_url = c['url']
+        image = c['images'][image_idx]
+        image_title = image['title']
 
-def make_card_embed(c, image_idx):
-    cid = c['id']
-    name = c['name_en']
-    info_url = c['url']
-    image = c['images'][image_idx]
-    image_title = image['title']
+        url = image['url']
+        file_name = url[url.rfind("/") + 1:]
 
-    url = image['url']
-    file_name = url[url.rfind("/") + 1:]
-
-    header = '[{}] {} - {}'.format(cid, name, image_title)
-    image_url = 'https://storage.googleapis.com/mirubot/alimages/raw/{}'.format(file_name)
-
-    embed = discord.Embed()
-    embed.title = header
-    embed.url = info_url
-    embed.set_image(url=image_url)
-    embed.set_footer(text='Requester may click the reactions below to switch tabs')
-    return embed
+        embed = discord.Embed()
+        embed.title = f'[{cid}] {name} - {image_title}'
+        embed.url = BASE_URL.format(file_name)
+        embed.set_image(url=BASE_URL.format(file_name))
+        embed.set_footer(text='Requester may click the reactions below to switch tabs')
+        return embed
