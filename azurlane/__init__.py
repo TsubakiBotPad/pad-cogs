f9de32121c1730bf2bc238e456866392dc36864d
AA <chasehult@gmail.com>
2022-05-24 02:38:19 -0700

    Fix azurlane cog (#1695)

--- azurlane/__init__.py
+++ azurlane/__init__.py
@@ -1,9 +1,9 @@
-from .azurelane import AzureLane
+from .azurlane import AzurLane
 
 __red_end_user_data_statement__ = "No personal data is stored."
 
 
 def setup(bot):
-    n = AzureLane(bot)
+    n = AzurLane(bot)
     bot.add_cog(n)
     bot.loop.create_task(n.reload_al())
