6d8febbbf7d1461fa4454359c432298174bdec46
River <18037011+RheingoldRiver@users.noreply.github.com>
2022-05-25 18:52:49 -0500

    Remove schoolidol cog, it's unused (#1696)

--- schoolidol/__init__.py
+++
@@ -1,9 +0,0 @@
-from .schoolidol import SchoolIdol
-
-__red_end_user_data_statement__ = "No personal data is stored."
-
-
-def setup(bot):
-    n = SchoolIdol(bot)
-    bot.add_cog(n)
-    bot.loop.create_task(n.reload_sif())
