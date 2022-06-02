fe5ff38319f7bcd4dc211d73c445be552a8e2676
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-06-02 01:17:52 -0500

    fix menu2

--- padle/closable_embed.py
+++ padle/closable_embed.py
@@ -1,6 +1,7 @@
 from tsutils.menu.closable_embed_base import ClosableEmbedMenuBase
 from padle.confirmation import PADleMonsterConfirmationView
 
+
 class ClosableEmbedMenu(ClosableEmbedMenuBase):
     MENU_TYPE = PADleMonsterConfirmationView.VIEW_TYPE
     view_types = {
--- padle/confirmation.py
+++ padle/confirmation.py
@@ -1,14 +1,18 @@
+from tsutils.menu.components.footers import embed_footer_with_state
 from tsutils.menu.view.closable_embed import ClosableEmbedViewState
 from discordmenu.embed.view import EmbedView
 from discordmenu.embed.components import EmbedMain
 from discord import Color as c
 
+
 class PADleMonsterConfirmationViewProps:
     def __init__(self, title: str):
         self.title = title
 
+
 class PADleMonsterConfirmationView:
     VIEW_TYPE = 'PADleConfirmation'
+
     @classmethod
     def embed(cls, state: ClosableEmbedViewState, props: PADleMonsterConfirmationViewProps):
         return EmbedView(
@@ -16,4 +20,5 @@ class PADleMonsterConfirmationView:
                 color=c.red(),
                 title=props.title,
             ),
+            embed_footer=embed_footer_with_state(state)
         )
