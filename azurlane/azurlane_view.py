d60b6f5ff0cd31265079273bdc1965a7b5c53993
AA <chasehult@gmail.com>
2022-06-06 02:49:21 -0700

    Use azurlane URL rather than cloudfront

--- azurlane/azurlane_view.py
+++ azurlane/azurlane_view.py
@@ -1,13 +1,11 @@
 from typing import Collection, TYPE_CHECKING
 
-from discordmenu.embed.components import EmbedMain, EmbedBodyImage
+from discordmenu.embed.components import EmbedBodyImage, EmbedMain
 from discordmenu.embed.view import EmbedView
 from tsutils.menu.components.config import UserConfig
 from tsutils.menu.components.footers import embed_footer_with_state
 from tsutils.menu.view.view_state_base import ViewStateBase
 from tsutils.query_settings.query_settings import QuerySettings
-
-from azurlane.resources import BASE_URL
 
 if TYPE_CHECKING:
     from dbcog.models.monster_model import MonsterModel
@@ -70,15 +68,13 @@ class AzurlaneView:
         image_title = image['title']
 
         url = image['url']
-        file_name = url[url.rfind("/") + 1:]
-        full_url = BASE_URL.format(file_name)
 
         return EmbedView(
             EmbedMain(
                 color=state.query_settings.embedcolor,
                 title=f'[{cid}] {name} - {image_title}',
-                url=full_url
+                url=url
             ),
-            embed_body_image=EmbedBodyImage(full_url),
+            embed_body_image=EmbedBodyImage(url),
             embed_footer=embed_footer_with_state(state)
         )
