abf17de6af65b3bcb173413717884dfc610bbb9e
AA <chasehult@gmail.com>
2022-06-24 09:53:51 -0700

    Add dungeons to PadMonitor (#1754)

--- padmonitor/padmonitor.py
+++ padmonitor/padmonitor.py
@@ -1,30 +1,32 @@
 import asyncio
 import logging
-
-import discord
 from io import BytesIO
-
-from redbot.core import checks, commands
-from redbot.core.utils.chat_formatting import box, pagify
-
 from typing import Dict, List, Optional, TYPE_CHECKING
 
-from tsutils.cog_settings import CogSettings
+import discord
+from redbot.core import Config, checks, commands
+from redbot.core.utils.chat_formatting import box, pagify
 from tsutils.formatting import contains_ja
 
 if TYPE_CHECKING:
-    from dbcog.database_context import DbContext
     from dbcog.models.monster_model import MonsterModel
+    from dbcog.models.dungeon_model import DungeonModel
 
 logger = logging.getLogger('red.padbot-cogs.padmonitor')
 
 
 class PadMonitor(commands.Cog):
     """Monitor PAD and announce new cards"""
+
     def __init__(self, bot, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bot = bot
-        self.settings = PadMonitorSettings("padmonitor")
+
+        self.config = Config.get_conf(self, identifier=9407071702)
+        self.config.register_global(seen_jp_monsters=[], seen_na_monsters=[],
+                                    monster_announce_channels=[],
+                                    seen_jp_dungeons=[], seen_na_dungeons=[],
+                                    dungeon_announce_channels=[], )
 
     async def red_get_data_for_user(self, *, user_id):
         """Get a user's personal data."""
@@ -42,27 +44,23 @@ class PadMonitor(commands.Cog):
         await self.bot.wait_until_ready()
         while self == self.bot.get_cog('PadMonitor'):
             try:
-                await self.check_seen()
-            except Exception as ex:
-                logger.exception("check seen loop caught exception " + str(ex))
+                await self.check_seen_monsters()
+                await self.check_seen_dungeons()
+            except Exception:
+                logger.exception("Error in PadMonitor loop:")
 
             await asyncio.sleep(60 * 5)
 
-    async def check_seen(self):
+    async def check_seen_monsters(self):
         """Refresh the monster indexes."""
-        dbcog = self.bot.get_cog('DBCog')
-        await dbcog.wait_until_ready()
-        all_monsters = [*dbcog.database.get_all_monsters()]
-        jp_monster_map = {m.monster_id: m for m in all_monsters if m.on_jp}
-        na_monster_map = {m.monster_id: m for m in all_monsters if m.on_na}
 
-        def process(existing: List[int], new_map: Dict[int, "MonsterModel"], name: str)\
-                -> Optional[str]:
+        def process(existing: List[int],  # Will be mutated
+                    new_map: Dict[int, "MonsterModel"],
+                    region: str) -> Optional[str]:
             if not existing:
                 # If everything is new, assume nothing is and log all current monsters as seen
-                logger.info('preloading %i', len(new_map))
+                logger.info('preloading %i monsters', len(new_map))
                 existing.extend(new_map.keys())
-                self.settings.save_settings()
                 return None
 
             delta_set = set(new_map.keys()).difference(existing)
@@ -72,29 +70,70 @@ class PadMonitor(commands.Cog):
                 return None
 
             existing.extend(delta_set)
-            self.settings.save_settings()
-            msg = 'New monsters added to {}:'.format(name)
+            msg = f'New monsters added to {region}:'
             for mid in sorted(delta_set):
                 m = new_map[mid]
                 msg += f'\n\t[{m.monster_id}] {m.unoverridden_name_en}'
-                if contains_ja(m.name_en) and m.name_en_override is not None:
+                if contains_ja(m.name_en) and m.name_en_override:
                     msg += f' ({m.name_en_override})'
             return msg
 
-        if (jp_results := process(self.settings.jp_seen(), jp_monster_map, 'JP')):
-            for channel_id in self.settings.new_monster_channels():
-                await self.announce(channel_id, jp_results)
-        if (na_results := process(self.settings.na_seen(), na_monster_map, 'NA')):
-            for channel_id in self.settings.new_monster_channels():
-                await self.announce(channel_id, na_results)
+        dbcog = self.bot.get_cog('DBCog')
+        await dbcog.wait_until_ready()
+        all_monsters = dbcog.database.get_all_monsters()
+
+        jp_monster_map = {m.monster_id: m for m in all_monsters if m.on_jp}
+        async with self.config.seen_jp_monsters() as jp_seen:
+            if (jp_results := process(jp_seen, jp_monster_map, 'JP')):
+                for channel_id in await self.config.monster_announce_channels():
+                    await self.announce(channel_id, jp_results)
+        na_monster_map = {m.monster_id: m for m in all_monsters if m.on_na}
+        async with self.config.seen_na_monsters() as na_seen:
+            if (na_results := process(na_seen, na_monster_map, 'NA')):
+                for channel_id in await self.config.monster_announce_channels():
+                    await self.announce(channel_id, na_results)
+
+    async def check_seen_dungeons(self):
+        """Refresh the dungeons indexes."""
+
+        def process(existing: List[int],  # Will be mutated
+                    new_map: Dict[int, "DungeonModel"]) -> Optional[str]:
+            # TODO: Don't repeat so much code here
+            if not existing:
+                # If everything is new, assume nothing is and log all current dungeons as seen
+                logger.info('preloading %i dungeons', len(new_map))
+                existing.extend(new_map.keys())
+                return None
+
+            delta_set = set(new_map.keys()).difference(existing)
+
+            if not delta_set:
+                # There are no new dungeons
+                return None
+
+            existing.extend(delta_set)
+            msg = f'New dungeons added:'
+            for mid in sorted(delta_set):
+                msg += f'\n\t{new_map[mid].name_en}'
+            return msg
+
+        dbcog = self.bot.get_cog('DBCog')
+        await dbcog.wait_until_ready()
+        all_dungeons = dbcog.database.dungeon.get_all_dungeons()
+
+        dungeon_map = {m.dungeon_id: m for m in all_dungeons}  # TODO: Find a way to test server
+        async with self.config.seen_jp_dungeons() as seen:
+            if (results := process(seen, dungeon_map)):
+                for channel_id in await self.config.dungeon_announce_channels():
+                    await self.announce(channel_id, results)
 
     async def announce(self, channel_id: int, message: str):
         try:
-            channel = self.bot.get_channel(int(channel_id))
+            channel = self.bot.get_channel(channel_id)
             for page in pagify(message):
                 await channel.send(box(page))
         except Exception as ex:
-            logger.exception('failed to send message to {}:'.format(channel_id))
+            logger.exception(f'Failed to send message to {channel_id}:')
 
     @commands.group(aliases=['pdm'])
     async def padmonitor(self, ctx):
@@ -106,68 +145,64 @@ class PadMonitor(commands.Cog):
     async def reload(self, ctx):
         """Update PDM channels"""
         async with ctx.typing():
-            await self.check_seen()
+            await self.check_seen_monsters()
+            await self.check_seen_dungeons()
         await ctx.tick()
 
-    @padmonitor.command()
+    @padmonitor.group(name='monsters', aliases=['monster'])
+    async def pdm_monsters(self, ctx):
+        """New monster announcement"""
+        pass
+
+    @pdm_monsters.command(name='addchannel')
     @commands.guild_only()
     @checks.mod_or_permissions(manage_guild=True)
-    async def addchannel(self, ctx, channel: discord.TextChannel = None):
+    async def pdm_m_addchannel(self, ctx, channel: discord.TextChannel = None):
         """Sets announcements for the current channel."""
-        if channel is None:
-            channel = ctx.channel
-        self.settings.add_new_monster_channel(channel.id)
+        channel = channel or ctx.channel
+        async with self.config.monster_announce_channels() as announce_channels:
+            if channel.id in announce_channels:
+                return await ctx.send(f"{channel} is already a monster announcement channel.")
+            announce_channels.append(channel.id)
         await ctx.tick()
 
-    @padmonitor.command()
+    @pdm_monsters.command(name='rmchannel', aliases=['removechannel'])
     @commands.guild_only()
     @checks.mod_or_permissions(manage_guild=True)
-    async def rmchannel(self, ctx, channel: discord.TextChannel = None):
+    async def pdm_m_rmchannel(self, ctx, channel: discord.TextChannel = None):
         """Removes announcements for the current channel."""
-        if channel is None:
-            channel = ctx.channel
-        self.settings.rm_new_monster_channel(channel.id)
+        channel = channel or ctx.channel
+        async with self.config.monster_announce_channels() as announce_channels:
+            if channel.id not in announce_channels:
+                return await ctx.send(f"{channel} is not a monster announcement channel.")
+            announce_channels.remove(channel.id)
         await ctx.tick()
 
+    @padmonitor.group(name='dungeons', aliases=['dungeon'])
+    async def pdm_dungeons(self, ctx):
+        """New dungeon announcement"""
+        pass
 
-class PadMonitorSettings(CogSettings):
-    def make_default_settings(self):
-        config = {
-            'jp_seen_ids': [],
-            'na_seen_ids': [],
-            'new_monster_channels': [],
-        }
-        return config
+    @pdm_dungeons.command(name='addchannel')
+    @commands.guild_only()
+    @checks.mod_or_permissions(manage_guild=True)
+    async def pdm_d_addchannel(self, ctx, channel: discord.TextChannel = None):
+        """Sets announcements for the current channel."""
+        channel = channel or ctx.channel
+        async with self.config.dungeon_announce_channels() as announce_channels:
+            if channel.id in announce_channels:
+                return await ctx.send(f"{channel} is already a dungeon announcement channel.")
+            announce_channels.append(channel.id)
+        await ctx.tick()
 
-    def jp_seen(self) -> List[int]:
-        return self.bot_settings['jp_seen_ids']
-
-    def na_seen(self) -> List[int]:
-        return self.bot_settings['na_seen_ids']
-
-    def add_jp_seen(self, monster_id: int) -> None:
-        ids = self.jp_seen()
-        if monster_id not in ids:
-            ids.append(monster_id)
-            self.save_settings()
-
-    def add_na_seen(self, monster_id: int) -> None:
-        ids = self.na_seen()
-        if monster_id not in ids:
-            ids.append(monster_id)
-            self.save_settings()
-
-    def new_monster_channels(self) -> List[int]:
-        return self.bot_settings['new_monster_channels']
-
-    def add_new_monster_channel(self, channel_id: int) -> None:
-        channels = self.new_monster_channels()
-        if channel_id not in channels:
-            channels.append(channel_id)
-            self.save_settings()
-
-    def rm_new_monster_channel(self, channel_id: int) -> None:
-        channels = self.new_monster_channels()
-        if channel_id in channels:
-            channels.remove(channel_id)
-            self.save_settings()
+    @pdm_dungeons.command(name='rmchannel', aliases=['removechannel'])
+    @commands.guild_only()
+    @checks.mod_or_permissions(manage_guild=True)
+    async def pdm_d_rmchannel(self, ctx, channel: discord.TextChannel = None):
+        """Removes announcements for the current channel."""
+        channel = channel or ctx.channel
+        async with self.config.dungeon_announce_channels() as announce_channels:
+            if channel.id not in announce_channels:
+                return await ctx.send(f"{channel} is not a dungeon announcement channel.")
+            announce_channels.remove(channel.id)
+        await ctx.tick()
