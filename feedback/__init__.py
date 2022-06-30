9e0e4daac93a7c8ac7a36a1cbe48f24aebb762b6
AA <chasehult@gmail.com>
2022-06-29 19:52:10 -0700

    Update to Dev (#1709)

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -3,5 +3,5 @@ from .feedback import Feedback
 __red_end_user_data_statement__ = "No personal data is stored."
 
 
-def setup(bot):
-    bot.add_cog(Feedback(bot))
+async def setup(bot):
+    bot.add_cog(Feedback(bot)) if not __import__('asyncio').iscoroutinefunction(bot.add_cog) else await bot.add_cog(Feedback(bot))
--- padsearch/__init__.py
+++ padsearch/__init__.py
@@ -3,5 +3,5 @@ from .padsearch import PadSearch
 __red_end_user_data_statement__ = "No personal data is stored."
 
 
-def setup(bot):
-    bot.add_cog(PadSearch(bot))
+async def setup(bot):
+    bot.add_cog(PadSearch(bot)) if not __import__('asyncio').iscoroutinefunction(bot.add_cog) else await bot.add_cog(PadSearch(bot))
