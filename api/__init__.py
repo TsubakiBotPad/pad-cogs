32b3bdb293d85b9c71062a3dfbc1ee5f80c7ee38
Ethan Goh <ethan@ethangoh.com>
2022-09-26 17:18:06 -0700

    rename apicog to api

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-from apicog.apicog import APICog
+from api.apicog import APICog
 
 with open(Path(__file__).parent / "info.json") as file:
     __red_end_user_data_statement__ = json.load(file)['end_user_data_statement']
