19b53b18e657747beacd2d734e86720a8ea37023
Ethan <ethananhphan@gmail.com>
2022-11-05 22:14:22 -0400

    Add score listener

---
+++ scorelistener/__init__.py
@@ -0,0 +1,7 @@
+from .scorelistener import ScoreListener
+
+__red_end_user_data_statement__ = "No personal data is stored."
+
+
+async def setup(bot):
+    bot.add_cog(ScoreListener(bot)) if not __import__('asyncio').iscoroutinefunction(bot.add_cog) else await bot.add_cog(ScoreListener(bot))
