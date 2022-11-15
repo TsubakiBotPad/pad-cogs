1db5a61ccea787b8156eea003e49cf9276d8c5c2
River <18037011+RheingoldRiver@users.noreply.github.com>
2022-11-14 21:05:17 -0600

    Add jpyt command & refactor dungeon lists (#1847)

---
+++ padinfo/view/dungeon_list/jpytdglead.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING
+
+from discordmenu.embed.text import LinkedText
+from tsutils.tsubaki.monster_header import MonsterHeader
+
+from padinfo.view.dungeon_list.dungeon_list import DungeonListBase, DungeonListViewProps
+
+if TYPE_CHECKING:
+    from dbcog.models.monster_model import MonsterModel
+
+
+class JpYtDgLeadProps(DungeonListViewProps):
+    def __init__(self, dungeons, monster: "MonsterModel"):
+        super().__init__(dungeons)
+        self.monster = monster
+
+
+class JpYtDgLeadView(DungeonListBase):
+    VIEW_TYPE = 'JpYtDgLead'
+    dungeon_link = 'https://www.youtube.com/results?search_query={}%20{}'
+    subdungeon_link = 'https://www.youtube.com/results?search_query={}%20{}'
+    bad_chars = []
+
+    @classmethod
+    def format_dg_link(cls, dungeon, props: JpYtDgLeadProps):
+        return LinkedText(cls.escape_name(dungeon['name_en']),
+                          cls.dungeon_link.format(dungeon['name'], props.monster.name_ja).replace(' ', '%20').replace('　','%20'))
+
+    @classmethod
+    def format_sd_link(cls, subdungeon, props: JpYtDgLeadProps):
+        return cls.subdungeon_link.format(subdungeon['name'].replace(' ', '%20').replace('　','%20'), props.monster.name_ja)
+
+    @classmethod
+    def print_name(cls, subdungeon, props: JpYtDgLeadProps):
+        return LinkedText(cls.escape_name(subdungeon['name_en']),
+                          cls.format_sd_link(subdungeon, props))
+
+    @classmethod
+    def description(cls, props: JpYtDgLeadProps):
+        return MonsterHeader.text_with_emoji(props.monster)
