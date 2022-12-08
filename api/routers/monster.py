70880f738ee025de3164a311906d1c2bd5eaa418
TsubakiBotPad <TsubakiBotPAD@gmail.com>
2022-12-08 09:39:37 +0800

    Add MonsterWithEvosResponse for team-builder

--- api/routers/monster.py
+++ api/routers/monster.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from api.botref import get_bot
-from api.responses.monster import MonsterResponse
+from api.responses.monster import MonsterResponse, MonsterWithEvosResponse
 
 monster_router = APIRouter()
 
@@ -11,3 +11,11 @@ async def get(monster_id):
     dbcog = get_bot().get_cog("DBCog")
     monster = await dbcog.find_monster(monster_id)
     return MonsterResponse.from_model(monster)
+
+
+@monster_router.get("/team-builder/{query}", response_model=MonsterResponse)
+async def get(query):
+    dbcog = get_bot().get_cog("DBCog")
+    monster = await dbcog.find_monster(query)
+    alt_monsters = dbcog.database.graph.get_alt_monsters(monster)
+    return MonsterWithEvosResponse.from_model(monster, alt_monsters)
