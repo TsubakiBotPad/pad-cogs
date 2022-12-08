27304d08ea75816acffb3013ba0dd027ebca01ca
TsubakiBotPad <TsubakiBotPAD@gmail.com>
2022-12-08 21:52:59 +0800

    6

--- api/routers/monster.py
+++ api/routers/monster.py
@@ -3,7 +3,7 @@ from typing import Union
 from fastapi import APIRouter, Query
 
 from api.botref import get_bot
-from api.responses.monster import MonsterResponse, MonsterWithEvosResponse
+from api.responses.monster import MonsterResponse, MonsterWithEvosResponse, MonstersResponse
 
 monster_router = APIRouter()
 
@@ -13,6 +13,19 @@ async def get(monster_id):
     dbcog = get_bot().get_cog("DBCog")
     monster = await dbcog.find_monster(monster_id)
     return MonsterResponse.from_model(monster)
+
+
+@monster_router.get("/", response_model=MonstersResponse)
+async def getManyById(q: Union[str, None] = Query(default=None, min_length=1)):
+    monster_ids = q.split(",")
+
+    dbcog = get_bot().get_cog("DBCog")
+    monsters = []
+    for monster_id in monster_ids:
+        monster = await dbcog.find_monster(monster_id)
+        monsters.append(monster)
+
+    return MonstersResponse.from_model(monsters)
 
 
 @monster_router.get("/team-builder/", response_model=MonsterWithEvosResponse)
