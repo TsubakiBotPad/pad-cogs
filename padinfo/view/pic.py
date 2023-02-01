0ff6562a33e72fee9988ca2ffbd2d272d02659bc
River <18037011+RheingoldRiver@users.noreply.github.com>
2023-02-01 04:14:20 -0600

    Add HQ animation links (#1893)

--- padinfo/view/pic.py
+++ padinfo/view/pic.py
@@ -1,7 +1,10 @@
+from typing import TYPE_CHECKING
+
 from discordmenu.embed.base import Box
 from discordmenu.embed.components import EmbedBodyImage, EmbedField, EmbedMain
 from discordmenu.embed.text import LinkedText, Text
 from discordmenu.embed.view import EmbedView
+from tsutils.formatting import filesize
 from tsutils.menu.components.footers import embed_footer_with_state
 from tsutils.tsubaki.links import MonsterImage, MonsterLink
 from tsutils.tsubaki.monster_header import MonsterHeader
@@ -9,6 +12,9 @@ from tsutils.tsubaki.monster_header import MonsterHeader
 from padinfo.view.base import BaseIdView
 from padinfo.view.components.evo_scroll_mixin import EvoScrollView
 from padinfo.view.components.view_state_base_id import ViewStateBaseId
+
+if TYPE_CHECKING:
+    from dbcog.models.monster_model import MonsterModel
 
 
 class PicViewState(ViewStateBaseId):
@@ -25,28 +31,44 @@ class PicView(BaseIdView, EvoScrollView):
     VIEW_TYPE = 'Pic'
 
     @classmethod
-    def embed(cls, state: PicViewState):
-        url = MonsterImage.picture(state.monster.monster_id)
-        animated = state.monster.has_animation
-        fields = [EmbedField(
-            'Extra Links',
+    def animation_field(cls, m: "MonsterModel"):
+        if not m.has_animation:
+            return None
+        return EmbedField(
+            'Animation',
             Box(
                 Box(
-                    Text('Animation:'),
-                    LinkedText('(MP4)', MonsterImage.video(state.monster.monster_no_jp)),
+                    LinkedText(f'MP4 ({filesize(m.mp4_size)})', MonsterImage.video(m.monster_no_jp)),
                     Text('|'),
-                    LinkedText('(GIF)', MonsterImage.gif(state.monster.monster_no_jp)),
-                    delimiter=' '
-                ) if animated else None,
-                Box(
-                    Text('Orb Skin:'),
-                    LinkedText('Regular', MonsterImage.orb_skin(state.monster.orb_skin_id)),
+                    LinkedText(f'GIF ({filesize(m.gif_size)})', MonsterImage.gif(m.monster_no_jp)),
                     Text('|'),
-                    LinkedText('Color Blind', MonsterImage.orb_skin_colorblind(state.monster.orb_skin_id)),
+                    LinkedText(f'GIF ({filesize(m.hq_gif_size)})', MonsterImage.hq_gif(m.monster_no_jp)),
                     delimiter=' '
-                ) if state.monster.orb_skin_id else None,
+                )
+            ),
+        )
+
+    @staticmethod
+    def orb_skin_field(m: "MonsterModel"):
+        if m.orb_skin_id is None:
+            return None
+        return EmbedField(
+            'Orb Skin',
+            Box(
+                LinkedText('Regular', MonsterImage.orb_skin(m.orb_skin_id)),
+                Text('|'),
+                LinkedText('Color Blind', MonsterImage.orb_skin_colorblind(m.orb_skin_id)),
+                delimiter=' '
             )
-        ),
+        )
+
+    @classmethod
+    def embed(cls, state: PicViewState):
+        m = state.monster
+        url = MonsterImage.picture(state.monster.monster_id)
+        fields = [
+            cls.animation_field(m),
+            cls.orb_skin_field(m),
             cls.evos_embed_field(state)
         ]
 
